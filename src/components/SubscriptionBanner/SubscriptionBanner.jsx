be60fd17bd4ea8f58f936ea8ea04b2f0303fc050
Kyle Wan <kyle94024@gmail.com>
2025-06-18 00:49:48 -0700

    fixed shortname exception issue

--- src/components/SubscriptionBanner/SubscriptionBanner.jsx
+++ src/components/SubscriptionBanner/SubscriptionBanner.jsx
@@ -12,7 +12,7 @@ const SubscriptionBanner = () => {
     }, []);
 
     return (
-        <div className={`subscription-banner ${tenant.shortName === "CF" ? "background-alt" : ""} ${tenant.shortName === "Ashermans" ? "background-alt" : ""}`}>
+        <div className={`subscription-banner ${tenant.shortName === "CF" ? "background-alt" : ""} ${tenant.shortName === "Asherman's" ? "background-alt" : ""}`}>
             <h2 className="heading-tertiary w-800">Get in Touch</h2>
             <p className="body-large color-light-grey">
                 Reach out to us for any questions, partnerships, or support.

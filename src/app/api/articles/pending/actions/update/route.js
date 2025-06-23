86ed953a00c765529cd4e2a4cb703d996b540d19
Kyle Wan <kyle94024@gmail.com>
2025-06-22 20:44:06 -0400

    trying again

--- src/app/api/articles/pending/actions/update/route.js
+++ src/app/api/articles/pending/actions/update/route.js
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 
 // Only allow POST method
 export async function POST(req) {
-    const { id, title, tags, innertext, summary, article_link, image_url, authors, publication_date } =
+    const { id, title, tags, innertext, summary, article_link, image_url, authors, original_publication_date } =
         await req.json(); // Parse JSON body from the request
 
     try {
         // Execute the update query, including image_url
         await query(
-            "UPDATE pending_article SET title = $1, tags = $2, innertext = $3, summary = $4, article_link = $5, image_url = $6, authors = $7, publication_date = $8 WHERE id = $9",
-            [title, tags, innertext, summary, article_link, image_url, authors, publication_date, id]
+            "UPDATE pending_article SET title = $1, tags = $2, innertext = $3, summary = $4, article_link = $5, image_url = $6, authors = $7, original_publication_date = $8 WHERE id = $9",
+            [title, tags, innertext, summary, article_link, image_url, authors, original_publication_date, id]
         );
 
         return NextResponse.json({
